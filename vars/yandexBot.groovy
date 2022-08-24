6ae095149ec54a2ec9853f4a8c890bc1bce7a077
yanusa3 <alekseenko_yan@mail.ru>
2022-08-24 12:10:50 +0300

    Add gitignore Add important flag for send message

--- vars/yandexBot.groovy
+++ vars/yandexBot.groovy
@@ -1,13 +1,15 @@
 import groovy.json.JsonBuilder
 
 def send(chat_id, message,
+         important=false,
          botUrl="https://botapi.messenger.yandex.net/bot/v1/messages/sendText/",
          credentialsId="yandexbot_token") {
   println("Sending yandex bot message: ${message}")
   withCredentials([string(credentialsId: credentialsId, variable: 'YANDEX_BOT_TOKEN')]) {
     def request = [
       chat_id: chat_id,
-      text: message
+      text: message,
+      important: important
     ]
     httpRequest url: botUrl, requestBody: new JsonBuilder(request).toString(), httpMode: 'POST', customHeaders: [
             [name: 'content-type', value: 'application/json'],
